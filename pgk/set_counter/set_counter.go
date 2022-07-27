4db87a771eb4dad6a6b6742eb96ebd8be6a19c42
cwshkut <alexey.shkut@clockwise.software>
2022-07-27 07:36:42 +0000

    remove mutexes from SetCounter

--- pgk/set_counter/set_counter.go
+++ pgk/set_counter/set_counter.go
@@ -1,36 +1,28 @@
 package internal
 
 import (
-	"sync"
 	"sync/atomic"
 )
 
-// Thread-safe Set for storing keys.
+// Set for storing keys. Thread-unsafe.
 // Unlike the classic Set, SetCounter counts the balance of Store/Release calls for each key and does not allow to release unexisting keys.
 type SetCounter struct {
-	mx *sync.Mutex
-	m  map[string]*int64
+	m map[string]*int64
 }
 
 func NewSetCounter() SetCounter {
 	return SetCounter{
-		mx: &sync.Mutex{},
-		m:  map[string]*int64{},
+		m: map[string]*int64{},
 	}
 }
 
 func (tm *SetCounter) Get(key string) *int64 {
-	tm.mx.Lock()
-	defer tm.mx.Unlock()
 	value := tm.m[key]
 
 	return value
 }
 
 func (tm *SetCounter) Store(key string) (value *int64) {
-	tm.mx.Lock()
-	defer tm.mx.Unlock()
-
 	var exists bool
 
 	value, exists = tm.m[key]
@@ -45,9 +37,6 @@ func (tm *SetCounter) Store(key string) (value *int64) {
 }
 
 func (tm *SetCounter) Release(key string) *int64 {
-	tm.mx.Lock()
-	defer tm.mx.Unlock()
-
 	value, ok := tm.m[key]
 	if !ok {
 		panic("Attempt to release non-existing key. Please, review your code.")
