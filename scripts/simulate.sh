f6fde9cce9afb77483b39eeea876694b86aa1838
cwshkut <alexey.shkut@clockwise.software>
2022-08-11 11:05:23 +0000

    fix simulation script

--- scripts/simulate.sh
+++ scripts/simulate.sh
@@ -1,2 +1,2 @@
 mkdir -p pprof
-go build -o ./bin/simulation cmd/simulation/simulation.go && ./bin/simulation -cpuprofile "pprof/prof"
+go build -o ./bin/simulation cmd/simulation/simulation.go && chmod +x ./bin/simulation && ./bin/simulation -cpuprofile "pprof/prof"
