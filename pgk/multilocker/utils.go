3577440e8099c40496f92a83b509a6b1e5c4ad32
cwshkut <alexey.shkut@clockwise.software>
2022-08-21 13:30:09 +0000

    refactor utils

--- pgk/multilocker/utils.go
+++ pgk/multilocker/utils.go
@@ -2,48 +2,21 @@ package multilocker
 
 import (
 	"encoding/binary"
-	"sort"
-	"strings"
 	"unsafe"
 )
 
 type tokenRef uintptr
 
-const uintptrSize = int(unsafe.Sizeof(uintptr(0)))
+const tokenSizeBytes = int(unsafe.Sizeof(uintptr(0)))
 
-func newTokenBuffer(tokenSize int) []byte {
-	return make([]byte, tokenSize*uintptrSize)
+func newTokenBuffer(size int) []byte {
+	return make([]byte, size*tokenSizeBytes)
 }
 
-func concatTokenRefs(pointers []tokenRef, concatSlice []byte) string {
+func concatTokenRefs(pointers []tokenRef, buffer []byte) string {
 	for i, p := range pointers {
-		binary.PutVarint(concatSlice[i*uintptrSize:], int64(p))
+		binary.PutVarint(buffer[i*tokenSizeBytes:], int64(p))
 	}
 
-	return string(concatSlice[0 : len(pointers)*uintptrSize])
-}
-
-func truncateAfterPrefixes(input []string) []string {
-	if len(input) == 0 {
-		return []string{}
-	}
-
-	output := []string{}
-
-	sort.Strings(input)
-
-	for _, str := range input {
-		if len(output) == 0 {
-			output = append(output, str)
-			continue
-		}
-
-		if strings.Contains(str, output[len(output)-1]) {
-			continue
-		}
-
-		output = append(output, str)
-	}
-
-	return output
+	return string(buffer[0 : len(pointers)*tokenSizeBytes])
 }
