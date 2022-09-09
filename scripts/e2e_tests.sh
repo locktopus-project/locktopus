3a83633d871545dd0c4bc3ed5a517755d81e0471
cwshkut <alexey.shkut@clockwise.software>
2022-09-09 19:35:47 +0000

    refactor script

--- scripts/e2e_tests.sh
+++ scripts/e2e_tests.sh
@@ -5,7 +5,12 @@ docker-compose -f docker/e2e/docker-compose.yml build
 docker-compose -f docker/e2e/docker-compose.yml up -d server
 
 for var in v1; do
-    docker-compose -f docker/e2e/docker-compose.yml up --exit-code-from v1 v1
+    if docker-compose -f docker/e2e/docker-compose.yml up --exit-code-from $var $var; then
+        echo test $var passed
+    else
+        exit 1
+    fi
+
 done
 
 docker-compose -f docker/e2e/docker-compose.yml down
