4c89804fd84c733aea6d1784a42da93ac9520ee3
cwshkut <alexey.shkut@clockwise.software>
2022-09-20 07:22:30 +0000

    enhance server_docker script

--- scripts/server_docker.sh
+++ scripts/server_docker.sh
@@ -1,2 +1,2 @@
 # Builds and runs server with default settings inside a container
-docker build -t gearlock . && docker run gearlock
+docker build -t gearlock . && docker run -it --rm --net=gearlock --name server gearlock
